012b94cc5142a6cfc81c7afd01268befb7422fd9
Narenthar <80049709+Narenthar@users.noreply.github.com>
2022-07-16 22:55:35 +0530

    frontend deployment site update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const server = require('http').createServer(app);
 const PORT = process.env.PORT || 5001;
 const io = require('socket.io')(server,{
     cors:{
-        origin : 'http://localhost:3000',
+        origin : 'https://chatapp-narenthar.netlify.app/',
         methods : ['GET', 'POST']
     }
 })
